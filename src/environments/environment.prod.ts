7379522cc7ab6bb528bb064735d98ab4beef1c40
tomislav.markovica <tomislav.markovica@em2.hr>
2022-12-08 12:15:23 +0100

    Backend on heroku working

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: "https://erit-backend.herokuapp.com/",
+  token: '26b833448e300ddc72626f2f3ca6f94cb7af7bf61fb8db64daa3bfc84f41c501f5059e7c7613d6101043ea1762409e27d8be9f1eb019ed9cc2fe693a7a462af2d578d372cb973a0cfd82f7fd14891c0179ac0c4c26a5721fe7668af46d1ddc3e446208b687ed1d9421b8e8b9f843b3df7b1e0f34fe8f873aaf1e95e17d5854eb'
 };
